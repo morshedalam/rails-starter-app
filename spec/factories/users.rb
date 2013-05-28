b5b5489aeab53807c4a19f311f084948b1a270f9
Morshed Alam <morshed201@gmail.com>
2013-05-28 13:45:06 +0600

    write basic test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
+    first_name 'Morshed'
+    last_name 'Alam'
+    email 'morshed@nsbench.com'
+    password 'morshed'
+    password_confirmation 'morshed'
   end
 end
